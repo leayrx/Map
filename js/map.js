fa02abf6b59c2a50ad9c6eb7ed72e2423bd60773
leayrx <148762598+leayrx@users.noreply.github.com>
2026-02-06 15:43:47 +0000

    Ajout carte France, bonhomme bleu et bonhomme rouge

--- js/map.js
+++ js/map.js
@@ -6,7 +6,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors',
 }).addTo(map);
 
-// Icone bonhomme rouge pour position manuelle
+// Marker rouge pour position manuelle
 const redIcon = L.icon({
     iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
     shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
@@ -16,12 +16,14 @@ const redIcon = L.icon({
     shadowSize: [41, 41]
 });
 
-// Icone casque de pompier pour position réelle
-const helmetIcon = L.icon({
-    iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/12/Firefighter_Helmet_icon.png',
-    iconSize: [32, 32],
-    iconAnchor: [16, 32],
-    popupAnchor: [0, -32]
+// Marker bleu par défaut pour position réelle
+const blueIcon = L.icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png',
+    shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
 });
 
 // Marker pour position manuelle
@@ -36,8 +38,8 @@ map.locate({ setView: false, maxZoom: 16 });
 map.on('locationfound', e => {
     if (myPositionMarker) map.removeLayer(myPositionMarker);
 
-    myPositionMarker = L.marker(e.latlng, { icon: helmetIcon }).addTo(map)
-        .bindPopup("Vous êtes ici (pompier)").openPopup();
+    myPositionMarker = L.marker(e.latlng, { icon: blueIcon }).addTo(map)
+        .bindPopup("Vous êtes ici").openPopup();
 });
 
 map.on('locationerror', e => {
@@ -56,7 +58,7 @@ document.getElementById('add-marker').addEventListener('click', () => {
     personMarker = L.marker([lat, lng], { icon: redIcon }).addTo(map)
         .bindPopup("Bonhomme rouge").openPopup();
 
-    map.setView([lat, lng], 14); // zoom sur le bonhomme
+    map.setView([lat, lng], 14);
 });
 
 // Clic sur la carte pour ajouter marker rouge
