12d01742a82b3f8e4307f916b793766987ae4f11
leayrx <148762598+leayrx@users.noreply.github.com>
2026-02-06 17:36:03 +0000

    mise a jour lien google sheet

--- js/map.js
+++ js/map.js
@@ -25,7 +25,7 @@ const redIcon = L.icon({
 });
 
 // ==== URL Apps Script ====
-const webAppURL = "https://script.google.com/macros/s/AKfycbzCTVLzHGLXylHmxzw1EBgQh11Qp5x0V2IHUsCXWOZmym4XWK-XZ_bNIp2WneAMpPVb/exec"; // remplace par ton lien production
+const webAppURL = "https://script.google.com/macros/s/AKfycbytuq2_QeqdgaueYOIcg9TxhL_ydSYEzMNnqIUcEiDS9jYwN6r-aIGN_q4cBky4vTCP/exec"; // remplace par ton lien production
 
 const allMarkers = [];
 
